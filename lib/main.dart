83ebde72293fcfa1f996085352c34930748234ab
emirkutlugun77 <57583899+emirkutlugun77@users.noreply.github.com>
2021-01-24 10:43:32 +0300

    commentLines

--- lib/main.dart
+++ lib/main.dart
@@ -1,113 +1,238 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:math';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class HomePage extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _HomePageState createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _HomePageState extends State<HomePage> {
+  static List<int> snakePosition = [
+    15,
+    25,
+    35,
+    45,
+    55
+  ]; // yılanın başlangıç pozisyonu
+  int numberOfSquares = 760; //ekranı 760 parçaya bölüyoruz
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  static var randomNumber =
+      Random(); //dart math kütüphanesini kullanarak elma için yeni bir yer oluşturuyoruz
+  int food = randomNumber.nextInt(50);
+  void generateNewFood() {
+    food = randomNumber.nextInt(50);
+  }
+
+  void startGame() {
+    //alttaki start düğmesine basıncabu fonksiyon çalışıyor
+    snakePosition = [15, 25, 35, 45, 55];
+    const duration = const Duration(milliseconds: 300);
+    Timer.periodic(duration, (Timer timer) {
+      //timer fonksiyonu timer.periodic() constructor methodu ile çağırıldığında timer.cancel() methodu çağırılana
+      //kadar belirtilen zaman aralığında kendi bloğundaki işlemleri tekrar eder, örnek:duration=1 saniye ise her saniye en baştan çalışır
+      updateSnake();
+      if (gameOver()) {
+        timer
+            .cancel(); //eğer oyun biterse timerı durdurup yılanın hareket etmesini engelliyoruz
+        _showGameOverScreen();
+      }
     });
+  }
+
+  var direction =
+      'down'; //yılanın hareketeni yönlerle kontrol ediyoruz ve oyun başlarken yılan aşağı doğru iniyor.
+  void updateSnake() {
+    setState(() {
+      switch (direction) {
+        case 'down':
+          if (snakePosition.last > 740) {
+            //eğer yılan aşağı gidiyorsa ve en alta geldiyse yoluna en üstten devam eder,
+            //if döngüsü bu özel durumu kontrol ediyor. Not:760 tane kare var ve 20 tane sütun vardı yani tek bir yatayda 20 kare var,
+            //bu yüzden 740-760 arası en uç olur
+            //diğer durumların da açıklamaları çok benzer hepsini yazmıyorum
+            snakePosition.add(snakePosition.last + 20 - 760);
+          } else {
+            snakePosition.add(snakePosition.last + 20);
+          }
+
+          break;
+
+        case 'up':
+          if (snakePosition.last < 20) {
+            snakePosition.add(snakePosition.last - 20 + 760);
+          } else {
+            snakePosition.add(snakePosition.last - 20);
+          }
+          break;
+
+        case 'left':
+          if (snakePosition.last % 20 == 0) {
+            snakePosition.add(snakePosition.last - 1 + 20);
+          } else {
+            snakePosition.add(snakePosition.last - 1);
+          }
+
+          break;
+
+        case 'right':
+          if ((snakePosition.last + 1) % 20 == 0) {
+            snakePosition.add(snakePosition.last + 1 - 20);
+          } else {
+            snakePosition.add(snakePosition.last + 1);
+          }
+          break;
+
+        default:
+      }
+      //update metodunda her şekilde bir tane kare yılana ekleniyor, ama eğer yılan elmayı almadıysa bir parça da siliniyor yani boyutu aynı kalıyor
+      //aşağıdaki if kondisyonu sağlanırsa silme işlemi gerçekleşmiyor ve yılanın boyutu 1 artıyor.
+      if (snakePosition.last == food) {
+        generateNewFood();
+      } else {
+        snakePosition.removeAt(0);
+      }
+    });
+  }
+
+  //aşağıdaki algoritmayı kendiniz düşünmenizi istiyorum, eğer anlamadığınız bir yer olursa sorun bana
+  bool gameOver() {
+    for (int i = 0; i < snakePosition.length; i++) {
+      int count = 0;
+      for (int j = 0; j < snakePosition.length; j++) {
+        if (snakePosition[i] == snakePosition[j]) {
+          count += 1;
+        }
+        if (count == 2) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  //önceki derste gördüğümüz showDialog() fonksiyonunu kullandık, bu fonksiyon kullanıcı önüne bir pop-up window çıkartıyor
+  void _showGameOverScreen() {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('GAME OVER'),
+            content: Text('You\'re score: ' + snakePosition.length.toString()),
+            actions: <Widget>[
+              FlatButton(
+                child: Text('Play Again'),
+                onPressed: () {
+                  startGame();
+                  Navigator.of(context).pop();
+                },
+              )
+            ],
+          );
+        });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+      backgroundColor: Colors.black,
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: GestureDetector(
+              //kullanıcı hareketlerini kaydetmek ve bu hareketlere bağlı olarak yönü değiştirmek için GestureDetector widget'ına bağlı
+              //fonksiyonları kullanıyoruz, bu deltaları anlamaya çalışırken telefonu bir koordinat düzlemi gibi düşünebilirsiniz
+              onVerticalDragUpdate: (details) {
+                //eğer yılan yukarı dışında herhangi bir yöne gidiyorsa ve değişim aşağı doğruysa
+                //(yani elinizi aşağı kaydırırsanız yönünüz 'down' olarak değişir)
+                //diğerleri için de benzer durumlar söz konusu
+                if (direction != 'up' && details.delta.dy > 0) {
+                  direction = 'down';
+                } else if (direction != 'down' && details.delta.dy < 0) {
+                  direction = 'up';
+                }
+              },
+              onHorizontalDragUpdate: (details) {
+                if (direction != 'left' && details.delta.dx > 0) {
+                  direction = 'right';
+                } else if (direction != 'right' && details.delta.dx < 0) {
+                  direction = 'left';
+                }
+              },
+              child: Container(
+                child: GridView.builder(
+                    physics:
+                        NeverScrollableScrollPhysics(), //bu kısım yazıldığı sürece gridView ne kadar uzun olursa olsun ekranı aşağı kaydıramazsınız
+                    itemCount: 760, //GridView da kaç tane kare olucak
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 20), //kaç tane sütun olucak
+                    itemBuilder: (BuildContext context, int index) {
+                      //item builder içerisinde mantıksal işlemler yapabilirsiniz,
+                      //burda her kareye atanan bir index var, ve biz bu index numberlarını kullanarak, kareleri boyuyoruz
+                      //yeşile boyanan kareler yılanı, kırmızılar elmayı ve siyah olanlar boş alanları temsil etmekte
+                      //bu kareleri boyarken updateSnake() fonksiyonunda güncellediğimiz sayıları kullanıyoruz
+                      //örnek olarak yılan [25,45,65,85,125 ] ise bu index numbera sahip kareleri boyuyoruz
+                      //timer fonksiyonunda yaptığımız işlemleri setState(){} methodu içinde  aldığımız için bu kısım da anlık olarak değişiyorr
+
+                      if (snakePosition.contains(index)) {
+                        return Center(
+                          child: snakeContainer(Colors.green),
+                        );
+                      }
+                      if (index == food) {
+                        return snakeContainer(Colors.red);
+                      } else {
+                        return snakeContainer(Colors.grey[900]);
+                      }
+                    }),
+              ),
+            ),
+          ),
+
+          //START BUTONU
+          Padding(
+            padding:
+                const EdgeInsets.only(bottom: 20.0, left: 20.0, right: 20.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                GestureDetector(
+                  onTap: startGame,
+                  child: Text(
+                    's t a r t',
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                ),
+              ],
+            ),
+          )
+          //START BUTONU
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+    );
+  }
+
+  //GridView de kullanılan her bir kare için UI kodu
+  Container snakeContainer(Color color) {
+    return Container(
+      padding: EdgeInsets.all(2),
+      child: ClipRRect(
+        child: Container(
+          color: color,
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
